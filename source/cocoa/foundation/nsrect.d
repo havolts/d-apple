3f53e2d238cfe1f3a76bf3350c6b62142d6298ce
havolts <datavolts1@gmail.com>
2026-08-30 22:21:10 +0100

    Potential fix for NSMakeRect.

--- source/cocoa/foundation/nsrect.d
+++ source/cocoa/foundation/nsrect.d
@@ -4,7 +4,7 @@ import coregraphics;
 
 alias NSRect = CGRect;
 
-NSRect NSMakeRect(int x, int y, int width, int height)
+public NSRect NSMakeRect(int x, int y, int width, int height)
 {
     return CGRect(CGPoint(x,y), CGSize(width, height));
 }
