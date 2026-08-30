5b1161c9c21c7362dfa4ba5a16f4272c6d85da18
havolts <datavolts1@gmail.com>
2026-08-30 22:18:16 +0100

    Added NSMakeRect method.

--- source/cocoa/foundation/nsrect.d
+++ source/cocoa/foundation/nsrect.d
@@ -3,3 +3,8 @@ module cocoa.foundation.nsrect;
 import coregraphics;
 
 alias NSRect = CGRect;
+
+NSRect NSMakeRect(int x, int y, int width, int height)
+{
+    return CGRect(CGPoint(x,y), CGSize(width, height));
+}
